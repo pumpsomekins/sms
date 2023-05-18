696fe952ead9ce50393c6c61332328f97bca52d8
pumpsomekins <sakuracorp@protonmail.com>
2023-05-18 17:11:10 -0400

    upload files

---
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplication.java
@@ -0,0 +1,11 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+}
